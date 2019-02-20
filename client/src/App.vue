21c6105ad65988ce949a564330197ca735bb7174
konstantin <mihrutka@inbox.ru>
2019-02-20 18:02:41 +0300

    доработки

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <nav class="navbar navbar-light bg-light">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <router-link to="/"
+                                 class="btn btn-primary btn-margin"
+                    >
+                        Home
+                    </router-link>
+
+                    <router-link to="/about"
+                                 class="btn btn-primary btn-margin"
+                    >
+                        About
+                    </router-link>
+
+                    <button
+                            class="btn btn-primary btn-margin"
+                            @click="login()">
+                        Log In
+                    </button>
+
+                    <button
+                            class="btn btn-primary btn-margin"
+                            @click="logout()">
+                        Log Out
+                    </button>
+
+                </div>
+            </div>
+        </nav>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+
+<script>
+    //import AuthService from './auth/AuthService'
+
+    // const auth = new AuthService();
+    //
+    // const { login, logout, authenticated, authNotifier } = auth;
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    #app {
+        width: 1000px;
+        margin: auto;
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
     }
-  }
-}
 </style>
